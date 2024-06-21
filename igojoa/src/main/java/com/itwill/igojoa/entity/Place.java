13136dc84d52773000abe1fe4e35771bcb876f7f
OGNOX <ohjinho0421@gmail.com>
2024-06-21 17:13:02 +0900

    테스트

---
+++ igojoa/src/main/java/com/itwill/igojoa/entity/Place.java
@@ -0,0 +1,17 @@
+package com.itwill.igojoa.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Place {
+	private String pName;
+	private String pLocation;
+	private String mon;
+	private String tue;
+}
