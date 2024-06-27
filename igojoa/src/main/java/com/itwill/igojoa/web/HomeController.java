6f26d4ff57758e7c1e60757884d0f2f7f6270ce4
OGNOX <ohjinho0421@gmail.com>
2024-06-27 14:03:28 +0900

    1

--- igojoa/src/main/java/com/itwill/igojoa/web/HomeController.java
+++ igojoa/src/main/java/com/itwill/igojoa/web/HomeController.java
@@ -9,21 +9,7 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 @Controller
 public class HomeController {
-<<<<<<< HEAD
-	@GetMapping("/")
-	public String home(HttpSession httpSession) {
-		log.debug("home??");
-		// 로그인 세션 값 있는지 확인
-		String userId = (String) httpSession.getAttribute("userId");
-		System.out.println(userId);
-		log.debug(userId);
-		// 있을때 실행될 리스트 서비스
 
-		// 없을때 실행될 리스트 서비스
-
-		return "home";
-	}
-=======
     @GetMapping("/")
     public String home(HttpSession session) {
         log.debug("home");
@@ -34,5 +20,4 @@ public class HomeController {
         System.out.println("세션에 저장된 아이디: " + userId);
         return "home";
     }
->>>>>>> 9b875e85cd97a4a96ca0c2c198ca4a12ab3f7f3a
 }
