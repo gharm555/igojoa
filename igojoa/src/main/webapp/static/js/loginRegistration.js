16ad00e5b3422569dd7620ee1a7f2b93f01bce44
gharm555 <phm2530@naver.com>
2024-07-01 17:13:44 +0900

    1

--- igojoa/src/main/webapp/static/js/loginRegistration.js
+++ igojoa/src/main/webapp/static/js/loginRegistration.js
@@ -120,8 +120,8 @@ function login() {
     .then((response) => {
       if (response.data.success) {
         console.log(response.data.pointsMessage);
-
         window.location.href = contextPath + "/";
+        console.log("왜이동함");
       } else {
         $loginCheckMessage.innerHTML =
           "아이디 또는 비밀번호를 잘못 입력했습니다.<br>입력하신 내용을 다시 확인해주세요.";
